69d57bbca7146fb8e7bedc81cd161d7c28460342
Igor Karatayev <igor.cwer@gmail.com>
2022-12-10 04:14:20 +0300

    update

--- ANSIConsole/ANSIInitializer.cs
+++ ANSIConsole/ANSIInitializer.cs
@@ -30,7 +30,8 @@ public class ANSIInitializer : IDisposable
 			if (printError) Console.WriteLine("failed to get output console mode");
 		}
 
-		var consoleMode = originalConsoleMode | ENABLE_VIRTUAL_TERMINAL_PROCESSING | DISABLE_NEWLINE_AUTO_RETURN;
+		var consoleMode = originalConsoleMode
+			| ENABLE_PROCESSED_OUTPUT | ENABLE_VIRTUAL_TERMINAL_PROCESSING | DISABLE_NEWLINE_AUTO_RETURN;
 		if (!SetConsoleMode(iStdOut, consoleMode))
 		{
 			if (printError) Console.WriteLine($"failed to set output console mode, error code: {GetLastError()}");
